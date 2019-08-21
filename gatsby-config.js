f8eaa167f06eb8c5f9a32367a7c718ce17e28025
Cristian Ospina <crisospina523@gmail.com>
2019-08-20 20:41:30 -0500

    add new plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,16 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Platziswag`,
     description: `El mejor swag de platzi disponible para ti`,
-    author: `@crisospina523`
+    author: `@crisospina523`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-stripe`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -25,6 +30,19 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
+    {
+      resolve: `gatsby-source-stripe`,
+      options: {
+        objects: ['Sku'],
+        secretKey: process.env.STRIPE_SK,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
