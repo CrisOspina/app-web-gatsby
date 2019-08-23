2c4e58f49602737cc5d713eb3c2fd5e594634854
Cristian Ospina <crisospina523@gmail.com>
2019-08-23 18:08:19 -0500

    + config pages for products

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,6 +17,7 @@ exports.createPages = async ({ graphql, actions }) => {
                 description
                 img
                 wear
+                color
               }
             }
           }
